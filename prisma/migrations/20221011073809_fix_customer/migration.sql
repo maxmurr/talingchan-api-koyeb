1efa40c1746957a129980cd956ec3425857ccf22
yongggnd <yongkerkwith@gmail.com>
2022-11-03 01:17:10 +0700

    remove migrate

--- prisma/migrations/20221011073809_fix_customer/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `tel` on the `Customer` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `VarChar(10)`.
-
-*/
--- AlterTable
-ALTER TABLE `Customer` MODIFY `tel` VARCHAR(10) NOT NULL;
